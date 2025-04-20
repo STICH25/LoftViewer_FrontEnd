340692fea383f2af9600e5856fa2f4db1ee72697
Ahmed Hernandez <ahernandez2501@gmail.com>
2025-04-20 00:45:50 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -32,7 +32,7 @@ export default defineConfig({
   resolve: {
     extensions: [".js", ".jsx"],
   },  
-  base: "./",
+  base: "/",
   build: {
     outDir: "dist",    
     input: "index.html", // Explicitly set entry file
